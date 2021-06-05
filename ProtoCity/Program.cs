5317414560800bbad050a88b68e0626582ac1436
vrtxt <goldenqubicle@gmail.com>
2021-06-06 01:24:25 +0200

    some restructure and other small changes

--- ProtoCity/Program.cs
+++ ProtoCity/Program.cs
@@ -16,28 +16,24 @@ namespace ProtoCity
         {
             new Circle
             {
-                Id = 0,
                 Position = new Vector2(Width / 2, Height / 2),
                 BaseColor = RED,
                 Radius = 5f
             },
             new Circle
             {
-                Id = 1,
                 Position = new Vector2(150, 150),
                 BaseColor = YELLOW,
                 Radius = 15f,
             },
             new Rectangle
             {
-                Id = 2,
                 Position = new Vector2(200, 300),
                 BaseColor = GREEN,
                 Size = new Vector2(20, 20)
             },
             new Polygon
             {
-                Id = 3,
                 Position = new Vector2(Width / 2, Height / 2),
                 BaseColor = BLUE,
                 TextCoords = Array.Empty<Vector2>(),
@@ -58,12 +54,13 @@ namespace ProtoCity
         {
             Mouse.Actions.Add(MouseEventHandler);
             KeyBoard.Actions.Add(KeyBoardEventHandler);
+
             InitWindow(Width, Height, Assembly.GetEntryAssembly().GetName().Name);
 
             while (!WindowShouldClose())
             {
-                foreach (var c in UIComponents)
-                    c.Update(GetMousePosition());
+                foreach (var comp in UIComponents)
+                    comp.Update(GetMousePosition());
 
                 Mouse.DoEvents();
                 KeyBoard.DoEvents();
@@ -74,14 +71,13 @@ namespace ProtoCity
             CloseWindow();
         }
 
-        internal static void UIEventHandler(UIEvent e)
+        internal static void UIEventHandler(IUIEvent e)
         {
             if (e is IEditEvent edit)
             {
                 if (edit is DeleteEdit d)
-                {
                     ToBeFlushed.Add(() => UIComponents.Remove(d.UIComponent));
-                }
+
                 UndoStack.Push(edit);
             }
         }
@@ -115,28 +111,20 @@ namespace ProtoCity
         private static void MouseEventHandler(IMouseEvent e)
         {
             foreach (var comp in UIComponents)
+            {
                 comp.OnMouseEvent(e);
+            }
         }
 
 
-        private static Texture2D texture2D = new() { id = 1, };
 
         private static void Draw()
         {
             BeginDrawing();
             ClearBackground(GRAY);
 
-            foreach (var e in UIComponents)
-            {
-                if (e is Circle c)
-                    DrawCircleV(c.Position, c.Radius, c.Color);
-
-                if (e is Rectangle r)
-                    DrawRectangleV(r.Position, r.Size, r.Color);
-
-                if (e is Polygon p)
-                    DrawTexturePoly(texture2D, p.Position, p.Points, p.TextCoords, p.Points.Length, p.Color);
-            };
+            foreach (var comp in UIComponents)
+                comp.Draw();
 
             EndDrawing();
         }
