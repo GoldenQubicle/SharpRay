aa85b5a6013c660c1b10f0a378e9dc1ccb6ff710
vrtxt <goldenqubicle@gmail.com>
2021-06-05 10:07:14 +0200

    mouse events

---
+++ ProtoCity/MouseEvent.cs
@@ -0,0 +1,59 @@
+﻿using Raylib_cs;
+using System;
+using System.Numerics;
+
+namespace ProtoCity
+{
+    public interface MouseEvent
+    {
+        public Vector2 Position { get; }
+    }
+
+    public struct MouseLeftClick : MouseEvent
+    {
+        public Vector2 Position { get; }
+        public MouseLeftClick(Vector2 position) => Position = position;
+        public override bool Equals(object obj) => obj is MouseLeftClick click && Position.Equals(click.Position);
+        public override int GetHashCode() => HashCode.Combine(Position);
+    }
+
+    public struct MouseMiddleClick : MouseEvent
+    {
+        public Vector2 Position { get; }
+        public MouseMiddleClick(Vector2 position) => Position = position;
+        public override bool Equals(object obj) => obj is MouseMiddleClick click && Position.Equals(click.Position);
+        public override int GetHashCode() => HashCode.Combine(Position);
+    }
+
+    public struct MouseRighttClick : MouseEvent
+    {
+        public Vector2 Position { get; }
+        public MouseRighttClick(Vector2 position) => Position = position;
+        public override bool Equals(object obj) => obj is MouseRighttClick click && Position.Equals(click.Position);
+        public override int GetHashCode() => HashCode.Combine(Position);
+    }
+
+    public struct MouseLeftDrag: MouseEvent
+    {
+        public Vector2 Position { get; }
+        public MouseLeftDrag(Vector2 position) => Position = position;
+        public override bool Equals(object obj) => obj is MouseLeftDrag click && Position.Equals(click.Position);
+        public override int GetHashCode() => HashCode.Combine(Position);
+    }
+
+    public struct MouseMiddleDrag : MouseEvent
+    {
+        public Vector2 Position { get; }
+        public MouseMiddleDrag(Vector2 position) => Position = position;
+        public override bool Equals(object obj) => obj is MouseMiddleDrag click && Position.Equals(click.Position);
+        public override int GetHashCode() => HashCode.Combine(Position);
+    }
+
+    public struct MouseRightDrag : MouseEvent
+    {
+        public Vector2 Position { get; }
+        public MouseRightDrag(Vector2 position) => Position = position;
+        public override bool Equals(object obj) => obj is MouseRightDrag click && Position.Equals(click.Position);
+        public override int GetHashCode() => HashCode.Combine(Position);
+    }
+}
--- ProtoCity/Program.cs
+++ ProtoCity/Program.cs
@@ -3,7 +3,8 @@ using static Raylib_cs.Color;
 using System.Reflection;
 using Raylib_cs;
 using System.Numerics;
-using System.Reflection.PortableExecutable;
+using System.Collections.Generic;
+using System;
 
 namespace ProtoCity
 {
@@ -11,50 +12,76 @@ namespace ProtoCity
     {
         private const int Width = 800;
         private const int Height = 480;
+
+        static List<Action<MouseEvent>> MouseSubscribers = new List<Action<MouseEvent>>();
+
         static void Main(string[] args)
         {
             InitWindow(Width, Height, Assembly.GetEntryAssembly().GetName().Name);
 
+            MouseSubscribers.Add(e => { if (e is MouseLeftClick) Console.WriteLine("left button"); });
+            MouseSubscribers.Add(e => { if (e is MouseMiddleClick) Console.WriteLine("middle button"); });
+            MouseSubscribers.Add(e => { if (e is MouseRighttClick) Console.WriteLine("right button"); });
+            MouseSubscribers.Add(e => { if (e is MouseLeftDrag) Console.WriteLine($"dragging left {e.Position}"); });
+
             while (!WindowShouldClose())
             {
+                DoMouseEvents();
                 Draw();
             }
 
             CloseWindow();
         }
 
-        static Vector2 center = new Vector2(Width / 4, Height / 4);
-        static bool IsOver = false;
         static bool IsDragging = false;
+        static Vector2 PreviousMousePostion;
+        static void DoMouseEvents()
+        {
+            var currentMousePostion = GetMousePosition();
+            IsDragging = currentMousePostion != PreviousMousePostion;
+
+            //click events
+            if (IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
+                MouseSubscribers.ForEach(ms => ms(new MouseLeftClick(currentMousePostion)));
+
+            if (IsMouseButtonPressed(MouseButton.MOUSE_MIDDLE_BUTTON))
+                MouseSubscribers.ForEach(ms => ms(new MouseMiddleClick(currentMousePostion)));
+
+            if (IsMouseButtonPressed(MouseButton.MOUSE_RIGHT_BUTTON))
+                MouseSubscribers.ForEach(ms => ms(new MouseRighttClick(currentMousePostion)));
+
+            //drag events
+            //send continuously even when cursor is outside window with potentially negative coordinates
+            if (IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON) && IsDragging)
+                MouseSubscribers.ForEach(ms => ms(new MouseLeftDrag(currentMousePostion)));
+
+            if (IsMouseButtonReleased(MouseButton.MOUSE_LEFT_BUTTON) && IsDragging)
+                IsDragging = false;
+
+            if (IsMouseButtonDown(MouseButton.MOUSE_MIDDLE_BUTTON) && IsDragging)
+                MouseSubscribers.ForEach(ms => ms(new MouseMiddleDrag(currentMousePostion)));
+
+            if (IsMouseButtonReleased(MouseButton.MOUSE_MIDDLE_BUTTON) && IsDragging)
+                IsDragging = false;
+
+            if (IsMouseButtonDown(MouseButton.MOUSE_RIGHT_BUTTON) && IsDragging)
+                MouseSubscribers.ForEach(ms => ms(new MouseRightDrag(currentMousePostion)));
+
+            if (IsMouseButtonReleased(MouseButton.MOUSE_RIGHT_BUTTON) && IsDragging)
+                IsDragging = false;
+
+
+            PreviousMousePostion = currentMousePostion;
+        }
+
+        static Vector2 center = new Vector2(Width / 4, Height / 4);
+
         static void Draw()
         {
             BeginDrawing();
             ClearBackground(GRAY);
-
-            var mPos = GetMousePosition();
             var radius = 10;
-
-            if (mPos.X > center.X - radius && mPos.X < center.X + radius &&
-                mPos.Y > center.Y - radius && mPos.Y < center.Y + radius)
-                IsOver = true;
-            else
-                IsOver = false;
-
-            if (IsOver && IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON))
-                IsDragging = true;
-
-            if (IsOver && IsMouseButtonReleased(MouseButton.MOUSE_LEFT_BUTTON))
-                IsDragging = false;
-
-            if (IsDragging)
-            {
-                center = mPos;
-                if (center.X < 0) center.X = 0;
-                if (center.X > Width) center.X = Width;
-                if (center.Y < 0) center.Y = 0;
-                if (center.Y > Height) center.Y = Height;
-            }
-
+          
             DrawCircleV(center, radius, GREEN);
             EndDrawing();
         }
