810e47b4e6cf7b284a79eb02aae186656e4f36fe
vrtxt <goldenqubicle@gmail.com>
2021-06-05 11:39:29 +0200

    move mouse event into class

---
+++ ProtoCity/Mouse.cs
@@ -0,0 +1,51 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using static Raylib_cs.Raylib;
+
+namespace ProtoCity
+{
+    public static class Mouse
+    {
+        public static List<Action<IMouseEvent>> Actions { get; } = new();
+
+        private static Vector2 PreviousMousePostion;
+        public static void DoEvents()
+        {
+            var currentMousePostion = GetMousePosition();
+            var isDragging = currentMousePostion != PreviousMousePostion;
+            var mouseWheel = GetMouseWheelMove();
+
+            //click events
+            if (IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
+                Actions.ForEach(ms => ms(new MouseLeftClick(currentMousePostion)));
+
+            if (IsMouseButtonPressed(MouseButton.MOUSE_MIDDLE_BUTTON))
+                Actions.ForEach(ms => ms(new MouseMiddleClick(currentMousePostion)));
+
+            if (IsMouseButtonPressed(MouseButton.MOUSE_RIGHT_BUTTON))
+                Actions.ForEach(ms => ms(new MouseRighttClick(currentMousePostion)));
+
+            //drag events
+            //send continuously even when cursor is outside window with potentially negative coordinates
+            if (IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON) && isDragging)
+                Actions.ForEach(ms => ms(new MouseLeftDrag(currentMousePostion)));
+
+            if (IsMouseButtonDown(MouseButton.MOUSE_MIDDLE_BUTTON) && isDragging)
+                Actions.ForEach(ms => ms(new MouseMiddleDrag(currentMousePostion)));
+
+            if (IsMouseButtonDown(MouseButton.MOUSE_RIGHT_BUTTON) && isDragging)
+                Actions.ForEach(ms => ms(new MouseRightDrag(currentMousePostion)));
+
+            //mousewheel events
+            if (mouseWheel == 1f)
+                Actions.ForEach(ms => ms(new MouseWheelUp(currentMousePostion)));
+
+            if (mouseWheel == -1f)
+                Actions.ForEach(ms => ms(new MouseWheelDown(currentMousePostion)));
+
+            PreviousMousePostion = currentMousePostion;
+        }
+    }
+}
--- ProtoCity/Program.cs
+++ ProtoCity/Program.cs
@@ -1,9 +1,7 @@
 ﻿using static Raylib_cs.Raylib;
 using static Raylib_cs.Color;
 using System.Reflection;
-using Raylib_cs;
 using System.Numerics;
-using System.Collections.Generic;
 using System;
 
 namespace ProtoCity
@@ -13,75 +11,24 @@ namespace ProtoCity
         private const int Width = 800;
         private const int Height = 480;
 
-        static List<Action<IMouseEvent>> MouseSubscribers = new List<Action<IMouseEvent>>();
-
         static void Main(string[] args)
         {
-            MouseSubscribers.Add(e => { if (e is MouseLeftClick) Console.WriteLine("left button"); });
-            MouseSubscribers.Add(e => { if (e is MouseMiddleClick) Console.WriteLine("middle button"); });
-            MouseSubscribers.Add(e => { if (e is MouseRighttClick) Console.WriteLine("right button"); });
-            MouseSubscribers.Add(e => { if (e is MouseLeftDrag) center = e.Position; });
-            MouseSubscribers.Add(e => { if (e is MouseWheelUp) Radius += 1.5f; });
-            MouseSubscribers.Add(e => { if (e is MouseWheelDown) Radius -= 1.5f; });
+            Mouse.Actions.Add(e => { if (e is MouseLeftClick) Console.WriteLine("left button"); });
+            Mouse.Actions.Add(e => { if (e is MouseMiddleClick) Console.WriteLine("middle button"); });
+            Mouse.Actions.Add(e => { if (e is MouseRighttClick) Console.WriteLine("right button"); });
+            Mouse.Actions.Add(e => { if (e is MouseLeftDrag) center = e.Position; });
+            Mouse.Actions.Add(e => { if (e is MouseWheelUp) Radius += 1.5f; });
+            Mouse.Actions.Add(e => { if (e is MouseWheelDown) Radius -= 1.5f; });
 
             InitWindow(Width, Height, Assembly.GetEntryAssembly().GetName().Name);
 
             while (!WindowShouldClose())
             {
-                DoMouseEvents();
+                Mouse.DoEvents();
                 Draw();
             }
 
             CloseWindow();
-        }
-
-        static bool IsDragging = false;
-        static Vector2 PreviousMousePostion;
-        static void DoMouseEvents()
-        {
-            var currentMousePostion = GetMousePosition();
-            var mouseWheel = GetMouseWheelMove();
-
-            IsDragging = currentMousePostion != PreviousMousePostion;
-
-            //click events
-            if (IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
-                MouseSubscribers.ForEach(ms => ms(new MouseLeftClick(currentMousePostion)));
-
-            if (IsMouseButtonPressed(MouseButton.MOUSE_MIDDLE_BUTTON))
-                MouseSubscribers.ForEach(ms => ms(new MouseMiddleClick(currentMousePostion)));
-
-            if (IsMouseButtonPressed(MouseButton.MOUSE_RIGHT_BUTTON))
-                MouseSubscribers.ForEach(ms => ms(new MouseRighttClick(currentMousePostion)));
-
-            //drag events
-            //send continuously even when cursor is outside window with potentially negative coordinates
-            if (IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON) && IsDragging)
-                MouseSubscribers.ForEach(ms => ms(new MouseLeftDrag(currentMousePostion)));
-
-            if (IsMouseButtonReleased(MouseButton.MOUSE_LEFT_BUTTON) && IsDragging)
-                IsDragging = false;
-
-            if (IsMouseButtonDown(MouseButton.MOUSE_MIDDLE_BUTTON) && IsDragging)
-                MouseSubscribers.ForEach(ms => ms(new MouseMiddleDrag(currentMousePostion)));
-
-            if (IsMouseButtonReleased(MouseButton.MOUSE_MIDDLE_BUTTON) && IsDragging)
-                IsDragging = false;
-
-            if (IsMouseButtonDown(MouseButton.MOUSE_RIGHT_BUTTON) && IsDragging)
-                MouseSubscribers.ForEach(ms => ms(new MouseRightDrag(currentMousePostion)));
-
-            if (IsMouseButtonReleased(MouseButton.MOUSE_RIGHT_BUTTON) && IsDragging)
-                IsDragging = false;
-
-            //mousewheel events
-            if (mouseWheel == 1f)
-                MouseSubscribers.ForEach(ms => ms(new MouseWheelUp(currentMousePostion)));
-
-            if(mouseWheel == -1f)
-                MouseSubscribers.ForEach(ms => ms(new MouseWheelDown(currentMousePostion)));
-
-            PreviousMousePostion = currentMousePostion;
         }
 
         static Vector2 center = new Vector2(Width / 4, Height / 4);
@@ -90,7 +37,7 @@ namespace ProtoCity
         {
             BeginDrawing();
             ClearBackground(GRAY);
-          
+
             DrawCircleV(center, Radius, GREEN);
             EndDrawing();
         }
