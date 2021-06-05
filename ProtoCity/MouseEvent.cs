28ba912256924931636091e3defe08ad844d5f2c
vrtxt <goldenqubicle@gmail.com>
2021-06-05 10:50:20 +0200

    more mouse events

--- ProtoCity/MouseEvent.cs
+++ ProtoCity/MouseEvent.cs
@@ -1,59 +1,98 @@
-﻿using Raylib_cs;
-using System;
+﻿using System;
 using System.Numerics;
 
 namespace ProtoCity
 {
-    public interface MouseEvent
+    public interface IMouseEvent
     {
         public Vector2 Position { get; }
     }
 
-    public struct MouseLeftClick : MouseEvent
+    public struct MouseLeftClick : IMouseEvent, IEquatable<MouseLeftClick>
     {
         public Vector2 Position { get; }
         public MouseLeftClick(Vector2 position) => Position = position;
+        public bool Equals(MouseLeftClick other) => Equals(other);
         public override bool Equals(object obj) => obj is MouseLeftClick click && Position.Equals(click.Position);
         public override int GetHashCode() => HashCode.Combine(Position);
+        public static bool operator ==(MouseLeftClick left, MouseLeftClick right) => left.Equals(right);
+        public static bool operator !=(MouseLeftClick left, MouseLeftClick right) => !left.Equals(right);
     }
 
-    public struct MouseMiddleClick : MouseEvent
+    public struct MouseMiddleClick : IMouseEvent, IEquatable<MouseMiddleClick>
     {
         public Vector2 Position { get; }
         public MouseMiddleClick(Vector2 position) => Position = position;
+        public bool Equals(MouseMiddleClick other) => Equals(other);
         public override bool Equals(object obj) => obj is MouseMiddleClick click && Position.Equals(click.Position);
         public override int GetHashCode() => HashCode.Combine(Position);
+        public static bool operator ==(MouseMiddleClick left, MouseMiddleClick right) => left.Equals(right);
+        public static bool operator !=(MouseMiddleClick left, MouseMiddleClick right) => !left.Equals(right);
     }
 
-    public struct MouseRighttClick : MouseEvent
+    public struct MouseRighttClick : IMouseEvent, IEquatable<MouseRighttClick>
     {
         public Vector2 Position { get; }
         public MouseRighttClick(Vector2 position) => Position = position;
+        public bool Equals(MouseRighttClick other) => Equals(other);
         public override bool Equals(object obj) => obj is MouseRighttClick click && Position.Equals(click.Position);
         public override int GetHashCode() => HashCode.Combine(Position);
+        public static bool operator ==(MouseRighttClick left, MouseRighttClick right) => left.Equals(right);
+        public static bool operator !=(MouseRighttClick left, MouseRighttClick right) => !left.Equals(right);
     }
 
-    public struct MouseLeftDrag: MouseEvent
+    public struct MouseLeftDrag: IMouseEvent, IEquatable<MouseLeftDrag>
     {
         public Vector2 Position { get; }
         public MouseLeftDrag(Vector2 position) => Position = position;
+        public bool Equals(MouseLeftDrag other) => Equals(other);
         public override bool Equals(object obj) => obj is MouseLeftDrag click && Position.Equals(click.Position);
         public override int GetHashCode() => HashCode.Combine(Position);
+        public static bool operator ==(MouseLeftDrag left, MouseLeftDrag right) => left.Equals(right);
+        public static bool operator !=(MouseLeftDrag left, MouseLeftDrag right) => !left.Equals(right);
     }
 
-    public struct MouseMiddleDrag : MouseEvent
+    public struct MouseMiddleDrag : IMouseEvent, IEquatable<MouseMiddleDrag>
     {
         public Vector2 Position { get; }
         public MouseMiddleDrag(Vector2 position) => Position = position;
+        public bool Equals(MouseMiddleDrag other) => Equals(other);
         public override bool Equals(object obj) => obj is MouseMiddleDrag click && Position.Equals(click.Position);
         public override int GetHashCode() => HashCode.Combine(Position);
+        public static bool operator ==(MouseMiddleDrag left, MouseMiddleDrag right) => left.Equals(right);
+        public static bool operator !=(MouseMiddleDrag left, MouseMiddleDrag right) => !left.Equals(right);
     }
 
-    public struct MouseRightDrag : MouseEvent
+    public struct MouseRightDrag : IMouseEvent, IEquatable<MouseRightDrag>
     {
         public Vector2 Position { get; }
         public MouseRightDrag(Vector2 position) => Position = position;
+        public bool Equals(MouseRightDrag other) => Equals(other);
         public override bool Equals(object obj) => obj is MouseRightDrag click && Position.Equals(click.Position);
         public override int GetHashCode() => HashCode.Combine(Position);
+        public static bool operator ==(MouseRightDrag left, MouseRightDrag right) => left.Equals(right);
+        public static bool operator !=(MouseRightDrag left, MouseRightDrag right) => !left.Equals(right);
+    }
+
+    public struct MouseWheelUp : IMouseEvent, IEquatable<MouseWheelUp>
+    {
+        public Vector2 Position { get; }
+        public MouseWheelUp(Vector2 position) => Position = position;
+        public bool Equals(MouseWheelUp other) => Equals(other);
+        public override bool Equals(object obj) => obj is MouseWheelUp click && Position.Equals(click.Position);
+        public override int GetHashCode() => HashCode.Combine(Position);
+        public static bool operator ==(MouseWheelUp left, MouseWheelUp right) => left.Equals(right);
+        public static bool operator !=(MouseWheelUp left, MouseWheelUp right) => !left.Equals(right);
+    }
+
+    public struct MouseWheelDown : IMouseEvent, IEquatable<MouseWheelDown>
+    {
+        public Vector2 Position { get; }
+        public MouseWheelDown(Vector2 position) => Position = position;
+        public bool Equals(MouseWheelDown other) => Equals(other);
+        public override bool Equals(object obj) => obj is MouseWheelDown click && Position.Equals(click.Position);
+        public override int GetHashCode() => HashCode.Combine(Position);
+        public static bool operator ==(MouseWheelDown left, MouseWheelDown right) => left.Equals(right);
+        public static bool operator !=(MouseWheelDown left, MouseWheelDown right) => !left.Equals(right);
     }
 }
--- ProtoCity/Program.cs
+++ ProtoCity/Program.cs
@@ -13,16 +13,18 @@ namespace ProtoCity
         private const int Width = 800;
         private const int Height = 480;
 
-        static List<Action<MouseEvent>> MouseSubscribers = new List<Action<MouseEvent>>();
+        static List<Action<IMouseEvent>> MouseSubscribers = new List<Action<IMouseEvent>>();
 
         static void Main(string[] args)
         {
-            InitWindow(Width, Height, Assembly.GetEntryAssembly().GetName().Name);
-
             MouseSubscribers.Add(e => { if (e is MouseLeftClick) Console.WriteLine("left button"); });
             MouseSubscribers.Add(e => { if (e is MouseMiddleClick) Console.WriteLine("middle button"); });
             MouseSubscribers.Add(e => { if (e is MouseRighttClick) Console.WriteLine("right button"); });
-            MouseSubscribers.Add(e => { if (e is MouseLeftDrag) Console.WriteLine($"dragging left {e.Position}"); });
+            MouseSubscribers.Add(e => { if (e is MouseLeftDrag) center = e.Position; });
+            MouseSubscribers.Add(e => { if (e is MouseWheelUp) Radius += 1.5f; });
+            MouseSubscribers.Add(e => { if (e is MouseWheelDown) Radius -= 1.5f; });
+
+            InitWindow(Width, Height, Assembly.GetEntryAssembly().GetName().Name);
 
             while (!WindowShouldClose())
             {
@@ -38,6 +40,8 @@ namespace ProtoCity
         static void DoMouseEvents()
         {
             var currentMousePostion = GetMousePosition();
+            var mouseWheel = GetMouseWheelMove();
+
             IsDragging = currentMousePostion != PreviousMousePostion;
 
             //click events
@@ -70,19 +74,24 @@ namespace ProtoCity
             if (IsMouseButtonReleased(MouseButton.MOUSE_RIGHT_BUTTON) && IsDragging)
                 IsDragging = false;
 
+            //mousewheel events
+            if (mouseWheel == 1f)
+                MouseSubscribers.ForEach(ms => ms(new MouseWheelUp(currentMousePostion)));
+
+            if(mouseWheel == -1f)
+                MouseSubscribers.ForEach(ms => ms(new MouseWheelDown(currentMousePostion)));
 
             PreviousMousePostion = currentMousePostion;
         }
 
         static Vector2 center = new Vector2(Width / 4, Height / 4);
-
+        static float Radius = 10f;
         static void Draw()
         {
             BeginDrawing();
             ClearBackground(GRAY);
-            var radius = 10;
           
-            DrawCircleV(center, radius, GREEN);
+            DrawCircleV(center, Radius, GREEN);
             EndDrawing();
         }
     }
