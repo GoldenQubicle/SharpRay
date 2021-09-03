fac045d4a74babc8e53e01637eaf162ef3555f75
vrtxt <goldenqubicle@gmail.com>
2021-09-03 12:08:26 +0200

    very basic zone drawing

---
+++ source/ProtoCity/Program.cs
@@ -0,0 +1,17 @@
+﻿using SharpRay.Core;
+using static SharpRay.Core.Application;
+
+namespace ProtoCity
+{
+    public static class Program
+    {
+        private static Zone newZone = new();
+
+        public static void Main(string[] args)
+        {
+            AddEntity(newZone);
+
+            Run(new Config { WindowWidth = 1080, WindowHeight = 720 });
+        }
+    }
+}
---
+++ source/ProtoCity/Zone.cs
@@ -0,0 +1,38 @@
+﻿using SharpRay.Core;
+using SharpRay.Entities;
+using SharpRay.Eventing;
+using System.Collections.Generic;
+using System.Numerics;
+using static Raylib_cs.Raylib;
+using Raylib_cs;
+using System.Linq;
+
+namespace ProtoCity
+{
+    public class Zone : Entity
+    {
+        private readonly List<Vector2> Points = new();
+
+        public override void Render()
+        {
+            foreach (var (p, i) in Points.Select((p, i) => (p, i)))
+            {
+                DrawCircleV(p, 3, Color.BEIGE);
+
+                if (i >= 1)
+                    DrawLineV(Points[i - 1], p, Color.BEIGE);
+
+                if (i == Points.Count - 1)
+                    DrawLineV(p, Points[0], Color.BEIGE);
+            }
+        }
+
+        public override void OnMouseEvent(IMouseEvent e)
+        {
+            if (e is MouseLeftClick mlc)
+            {
+                Points.Add(mlc.Position);
+            }
+        }
+    }
+}
