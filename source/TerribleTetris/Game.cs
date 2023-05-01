241021ec126cd31aa2d98ed4b858283ce5eb660c
vrtxt <goldenqubicle@gmail.com>
2023-05-01 18:46:30 +0200

    scaffold terrible tetris project

---
+++ source/TerribleTetris/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace TerribleTetris
+{
+	public static class Game
+	{
+		public static int WindowHeight => 480;
+
+		public static int WindowWidth => 680;
+
+		static void Main(string[ ] args)
+		{
+			Initialize(new SharpRayConfig { WindowWidth = WindowWidth, WindowHeight = WindowHeight, DoEventLogging = false, ShowFPS = true});
+			
+
+			Run( );
+		}
+	}
+}
