d60d653db7d35bb611ff7191d76dbd7cdcc4274a
vrtxt <goldenqubicle@gmail.com>
2023-05-05 14:36:30 +0200

    do count frames always & fixed some typos

--- source/TerribleTetris/Game.cs
+++ source/TerribleTetris/Game.cs
@@ -1,4 +1,6 @@
-﻿namespace TerribleTetris
+﻿using System.Text.Json;
+
+namespace TerribleTetris
 {
 	internal static partial class Game
 	{
@@ -8,28 +10,51 @@
 
 		internal enum Shape { I, O, T, J, L, S, Z };
 
-		internal enum Rotation { Up, Right, Down, Left}
+		internal enum Rotation { Up, Right, Down, Left }
 
 		internal enum RotationSystem { Super }
 
 		internal static void Main(string[ ] args)
 		{
+			SaveJson( );
+
 			Initialize(new SharpRayConfig
 			{
-				WindowWidth = WindowWidth, 
-				WindowHeight = WindowHeight, 
-				DoEventLogging = false, 
-				ShowFPS = true, 
+				WindowWidth = WindowWidth,
+				WindowHeight = WindowHeight,
+				DoEventLogging = false,
+				ShowFPS = true,
 				BackGroundColor = DARKGRAY
 			});
 
-			var gridData = new GridData(Rows: 16, Cols: 10, CellSize: 20, Color1: RAYWHITE, Color2: LIGHTGRAY );
+			var gridData = new GridData(Rows: 16, Cols: 10, CellSize: 20, Color1: RAYWHITE, Color2: LIGHTGRAY);
 			AddEntity(new Grid(gridData));
 
 			var tetrominoData = new TetrominoData(Shape.T);
 			AddEntity(new Tetromino(tetrominoData, gridData));
 
 			Run( );
+		}
+
+		private static void SaveJson()
+		{
+			var data = new Dictionary<RotationSystem, Dictionary<Shape, Dictionary<Rotation, List<Vector2>>>>
+			{
+				{RotationSystem.Super, new Dictionary<Shape, Dictionary<Rotation, List<Vector2>>>
+				{
+					{ Shape.T , new Dictionary<Rotation, List<Vector2>>
+						{
+							{ Rotation.Up , new( ) { new (1, 0), new(0, 1), new(1, 1), new(2, 1) } },
+							{ Rotation.Right, new ( ) { new(1,0), new(1,1), new(2,1), new(1,2) } },
+							{ Rotation.Down, new ( ) { new(0,1), new(1,1), new(2,1), new(1,2) } },
+							{ Rotation.Left , new ( ) { new(1,0), new(0,1), new(1,1), new(1,2) } }
+						}
+					}
+				}}
+			};
+
+			var json = JsonSerializer.Serialize(data);
+			File.WriteAllText(Path.Combine(AssestsFolder, "test.json"), json);
 		}
 	}
 }
--- source/TerribleTetris/Tetromino.cs
+++ source/TerribleTetris/Tetromino.cs
@@ -22,15 +22,12 @@ internal static partial class Game
 
 		public override void Render()
 		{
-
-
 			DrawRectangleLinesEx(_boundingBox, 3, BLUE);
 			foreach (var offset in _shape.Offsets[(Rotation)_rotation])
 			{
 				var pos = Position + new Vector2(offset.x * _grid.CellSize, offset.y * _grid.CellSize);
 				DrawRectangleV(pos, new Vector2(_grid.CellSize, _grid.CellSize), _shape.Color);
 			}
-
 		}
 
 		public override void Update(double deltaTime)
@@ -44,7 +41,7 @@ internal static partial class Game
 			{
 				KeyLeftReleased => _rotation == 0 ? 3 : _rotation - 1,
 				KeyRightReleased => ( _rotation + 1 ) % 4,
-				_ => +_rotation
+				_ => _rotation
 			};
 		}
 	}
