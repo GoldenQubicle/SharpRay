6a95404cab9db3939123cbd03c7d0c0d10259bd0
vrtxt <goldenqubicle@gmail.com>
2023-05-05 17:19:18 +0200

    tetromino rotation, movement & proper controls

--- source/SharpRay/Components/Easing.cs
+++ source/SharpRay/Components/Easing.cs
@@ -4,19 +4,31 @@ namespace SharpRay.Components
 {
     public class Easing : Component
     {
-        private readonly Func<float, float, float, float, float> EasingFunction;
-        private readonly double IntervalTime;
-        private bool IsReversed;
-        private readonly bool IsRepeated;
-        private double ElapsedTime;
+        private readonly Func<float, float, float, float, float> _easingFunction;
+        private readonly double _intervalTime;
+        private bool _isReversed;
+        private readonly bool _isRepeated;
+        private double _elapsedTime;
 
         public Easing(Func<float, float, float, float, float> easingFunction, double intervalTime, bool isReversed = false, bool isRepeated = false)
         {
-            EasingFunction = easingFunction;
-            IntervalTime = intervalTime * TickMultiplier;
-            IsReversed = isReversed;
-            IsRepeated = isRepeated;
+            _easingFunction = easingFunction;
+            _intervalTime = intervalTime * TickMultiplier;
+            _isReversed = isReversed;
+            _isRepeated = isRepeated;
         }
+
+        /// <summary>
+        /// Returns false when the easing is done, i.e. elapsed time is larger than interval time.
+        /// Will return false for repeating easings. 
+        /// </summary>
+        /// <returns></returns>
+        public bool IsDone() => !_isRepeated && _elapsedTime > _intervalTime;
+        
+        /// <summary>
+        /// Resets elapsed time to zero. 
+        /// </summary>
+        public void Reset() => _elapsedTime = 0;
 
         /// <summary>
         /// Set the elapsed interval time based on normalized value 0-1
@@ -24,8 +36,8 @@ namespace SharpRay.Components
         /// <param name="v"></param>
         public void SetElapsedTime(float v)
         {
-            var s = IsReversed ? 1 - v : v;
-            ElapsedTime = MapRange(s, 0d, 1d, 0d, IntervalTime);
+            var s = _isReversed ? 1 - v : v;
+            _elapsedTime = MapRange(s, 0d, 1d, 0d, _intervalTime);
         }
 
         /// <summary>
@@ -35,20 +47,20 @@ namespace SharpRay.Components
         /// <returns>float</returns>
         public float GetValue()
         {
-            var t = Math.Min(ElapsedTime, IntervalTime);
-            var e = EasingFunction((float)t, 0f, 1f, (float)IntervalTime);
-            return IsReversed ? 1 - e : e;
+            var t = Math.Min(_elapsedTime, _intervalTime);
+            var e = _easingFunction((float)t, 0f, 1f, (float)_intervalTime);
+            return _isReversed ? 1 - e : e;
         }
 
         public override void Update(double deltaTime)
         {
-            ElapsedTime += deltaTime;
+            _elapsedTime += deltaTime;
 
-            if (IsRepeated)
-                if (ElapsedTime > IntervalTime)
+            if (_isRepeated)
+                if (_elapsedTime > _intervalTime)
                 {
-                    ElapsedTime = 0;
-                    IsReversed = !IsReversed;
+                    _elapsedTime = 0;
+                    _isReversed = !_isReversed;
                 }
         }
     }
--- source/TerribleTetris/Tetromino.cs
+++ source/TerribleTetris/Tetromino.cs
@@ -8,21 +8,29 @@ internal static partial class Game
 	{
 		private readonly TetrominoData _shape;
 		private readonly GridData _grid;
-		private Rectangle _boundingBox;
 		private int _rotation;
+		private Easing _easing;
+		private float _nextY;
+		private float _oldY;
+		private float _x;
+
+		private Vector2 _bbSize;
 
 		public Tetromino(TetrominoData shape, GridData grid)
 		{
 			_shape = shape;
 			_grid = grid;
-			var bbSize = _shape.BoundingBoxSize * _grid.CellSize;
+			_bbSize = new(_shape.BoundingBoxSize * _grid.CellSize, _shape.BoundingBoxSize * _grid.CellSize);
 			Position = grid.Position;
-			_boundingBox = new Rectangle(Position.X, Position.Y, bbSize, bbSize);
+			_easing = new Easing(Easings.EaseBounceOut, LevelTimer);
+			_oldY = Position.Y;
+			_nextY = Position.Y + grid.CellSize;
+			_x = Position.X;
 		}
 
 		public override void Render()
 		{
-			DrawRectangleLinesEx(_boundingBox, 3, BLUE);
+			DrawRectangleLinesV(Position, _bbSize, BLUE);
 			foreach (var offset in _shape.Offsets[(Rotation)_rotation])
 			{
 				var pos = Position + new Vector2(offset.x * _grid.CellSize, offset.y * _grid.CellSize);
@@ -32,6 +40,17 @@ internal static partial class Game
 
 		public override void Update(double deltaTime)
 		{
+			_easing.Update(deltaTime);
+
+			if (_easing.IsDone( ))
+			{
+				_oldY = Position.Y;
+				_nextY = Position.Y + _grid.CellSize;
+				_easing.Reset( );
+				return;
+			}
+
+			Position = new Vector2(_x, MapRange(_easing.GetValue( ), 0f, 1f, _oldY, _nextY));
 
 		}
 
@@ -39,9 +58,18 @@ internal static partial class Game
 		{
 			_rotation = e switch
 			{
-				KeyLeftReleased => _rotation == 0 ? 3 : _rotation - 1,
-				KeyRightReleased => ( _rotation + 1 ) % 4,
+				KeyUpReleased or KeyPressed { KeyboardKey: KeyboardKey.KEY_X } => ( _rotation + 1 ) % 4,
+				KeyPressed { KeyboardKey: KeyboardKey.KEY_LEFT_CONTROL } or
+				KeyPressed { KeyboardKey: KeyboardKey.KEY_RIGHT_CONTROL } or
+				KeyPressed { KeyboardKey: KeyboardKey.KEY_Z } => _rotation == 0 ? 3 : _rotation - 1,
 				_ => _rotation
+			};
+
+			_x = e switch
+			{
+				KeyRightReleased => _x + _grid.CellSize,
+				KeyLeftReleased => _x - _grid.CellSize,
+				_ => _x
 			};
 		}
 	}
