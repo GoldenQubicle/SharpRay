07831a588f8e1b46d924a9cdb574c203b1d3c104
vrtxt <goldenqubicle@gmail.com>
2023-05-05 12:58:53 +0200

    added tetromino

--- source/TerribleTetris/Game.cs
+++ source/TerribleTetris/Game.cs
@@ -1,18 +1,29 @@
 ﻿namespace TerribleTetris
 {
-	internal static class Game
+	internal static partial class Game
 	{
 		internal static int WindowHeight => 480;
 
 		internal static int WindowWidth => 680;
 
-		internal enum Tetromino { I, O, T, J, L, S, Z };
+		internal enum Shape { I, O, T, J, L, S, Z };
 
 		internal static void Main(string[ ] args)
 		{
-			Initialize(new SharpRayConfig { WindowWidth = WindowWidth, WindowHeight = WindowHeight, DoEventLogging = false, ShowFPS = true });
+			Initialize(new SharpRayConfig
+			{
+				WindowWidth = WindowWidth, 
+				WindowHeight = WindowHeight, 
+				DoEventLogging = false, 
+				ShowFPS = true, 
+				BackGroundColor = DARKGRAY
+			});
 
-			AddEntity(new Grid(new GridData(Rows: 10, Cols: 5, CellSize: 35)));
+			var gridData = new GridData(Rows: 16, Cols: 10, CellSize: 20, Color1: RAYWHITE, Color2: LIGHTGRAY );
+			AddEntity(new Grid(gridData));
+
+			var tetrominoData = new TetrominoData(Shape.Z);
+			AddEntity(new Tetromino(tetrominoData, gridData));
 
 			Run( );
 		}
--- source/TerribleTetris/GridData.cs
+++ source/TerribleTetris/GridData.cs
@@ -1,8 +1,10 @@
 namespace TerribleTetris
 {
-	internal record GridData(int Rows, int Cols, int CellSize)
+	internal record GridData(int Rows, int Cols, int CellSize, Color Color1, Color Color2)
 	{
 		public int Height = Rows * CellSize;
 		public int Width = Cols * CellSize;
+		public Vector2 Position => new(( Game.WindowWidth - Width ) / 2, ( Game.WindowHeight - Height ) / 2);
+
 	};
 }
