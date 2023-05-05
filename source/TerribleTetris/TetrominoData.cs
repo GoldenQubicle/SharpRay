48f9247cca30457ce68a4d19795c04b4a702bdce
vrtxt <goldenqubicle@gmail.com>
2023-05-05 15:20:22 +0200

    added super rotations for all tetrominos

--- source/TerribleTetris/TetrominoData.cs
+++ source/TerribleTetris/TetrominoData.cs
@@ -25,8 +25,20 @@ internal static partial class Game
 
 		public IDictionary<Rotation, List<(int x, int y)>> Offsets = Shape switch
 		{
-			//Shape.I => new( ) { (0, 0), (1, 0), (2, 0), (3, 0) },
-			//Shape.O => new( ) { (0, 0), (1, 0), (0, 1), (1, 1) },
+			Shape.I => new Dictionary<Rotation, List<(int x, int y)>>
+			{
+				{ Rotation.Up , new( ) { (0,1), (1,1), (2,1), (3,1) } },
+				{ Rotation.Right , new( ) { (2,0), (2,1), (2,2), (2,3) } },
+				{ Rotation.Down , new( ) { (0,2), (1,2), (2,2), (3,2) } },
+				{ Rotation.Left , new( ) { (1,0), (1,1), (1,2), (1,3) } },
+			},
+			Shape.O => new Dictionary<Rotation, List<(int x, int y)>>
+			{
+				{ Rotation.Up , new( ) { (0,0), (0,1), (1,0), (1,1) } },
+				{ Rotation.Right , new( ) { (0, 0), (0, 1), (1, 0), (1, 1) } },
+				{ Rotation.Down , new( ) { (0, 0), (0, 1), (1, 0), (1, 1) } },
+				{ Rotation.Left , new( ) { (0, 0), (0, 1), (1, 0), (1, 1) } },
+			},
 			Shape.T => new Dictionary<Rotation, List<(int x, int y)>>
 			{
 				{ Rotation.Up , new( ) { (1, 0), (0, 1), (1, 1), (2, 1) } },
@@ -34,10 +46,34 @@ internal static partial class Game
 				{ Rotation.Down, new ( ) { (0,1), (1,1), (2,1), (1,2) } },
 				{ Rotation.Left , new ( ) { (1,0), (0,1), (1,1), (1,2) } }
 			},
-			//Shape.J => new( ) { (1, 0), (1, 1), (1, 2), (0, 2) },
-			//Shape.L => new( ) { (1, 0), (1, 1), (1, 2), (2, 2) },
-			//Shape.S => new( ) { (1, 0), (2, 0), (0, 1), (1, 1) },
-			//Shape.Z => new( ) { (0, 0), (1, 0), (1, 1), (2, 1) },
+			Shape.J => new Dictionary<Rotation, List<(int x, int y)>>
+			{
+				{ Rotation.Up , new( ) { (0,0), (0,1), (1,1), (2,1) } },
+				{ Rotation.Right , new( ) { (1,0), (2,0), (1,1), (1,2) } },
+				{ Rotation.Down , new( ) { (0,1), (1,1), (2,1), (2,2) } },
+				{ Rotation.Left , new( ) { (0,2), (1,0), (1,1), (1,2) } },
+			},
+			Shape.L => new Dictionary<Rotation, List<(int x, int y)>>
+			{
+				{ Rotation.Up , new( ) { (0,1), (1,1), (2,1), (2,0) } },
+				{ Rotation.Right , new( ) { (1,0), (1,1), (1,2), (2,2) } },
+				{ Rotation.Down , new( ) { (0,1), (0,2), (1,1), (2,1) } },
+				{ Rotation.Left , new( ) { (0,0), (1,0), (1,1), (1,2) } },
+			},
+			Shape.S => new Dictionary<Rotation, List<(int x, int y)>>
+			{
+				{ Rotation.Up , new( ) { (1,0), (2,0), (0,1), (1,1) } },
+				{ Rotation.Right , new( ) { (1,0), (1,1), (2,1), (2,2) } },
+				{ Rotation.Down , new( ) { (0,2), (1,1), (1,2), (2,1) } },
+				{ Rotation.Left , new( ) { (0,0), (0,1), (1,1), (1,2) } },
+			},
+			Shape.Z => new Dictionary<Rotation, List<(int x, int y)>>
+			{
+				{ Rotation.Up , new( ) { (0,0), (1,0), (1,1), (2,1) } },
+				{ Rotation.Right , new( ) { (2,0), (1,1), (2,1), (1,2) } },
+				{ Rotation.Down , new( ) { (0,1), (1,1), (1,2), (2,2) } },
+				{ Rotation.Left , new( ) { (0,1), (0,2), (1,0), (1,1) } },
+			},
 			_ => throw new ArgumentOutOfRangeException(nameof(Shape), Shape, null)
 		};
 	}
