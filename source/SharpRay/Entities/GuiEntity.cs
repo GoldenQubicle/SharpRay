658101148af24a7c6cab8043802e794b622fc9ff
Erik van Ekeren <e.van.ekeren@betabit.nl>
2024-01-09 13:13:45 +0100

    start visualization 2017 day 10

--- source/SharpRay/Entities/GuiEntity.cs
+++ source/SharpRay/Entities/GuiEntity.cs
@@ -9,8 +9,22 @@
         public Func<GuiEntity, IGuiEvent> OnMouseLeftClick { get; set; }
         public float Scale { get; set; } = 1f;
         public bool HasMouseFocus { get; set; }
+        public bool IsVisible { get; set; } = true;
+        public void Show() => IsVisible = true;
+
+        public void Hide()
+        {
+	        IsVisible = false;
+	        HasMouseFocus = false;
+        } 
         public virtual bool ContainsPoint(Vector2 point) => false;
-        public override void OnMouseEvent(IMouseEvent e) => HasMouseFocus = ContainsPoint(e.Position);
+        public override void OnMouseEvent(IMouseEvent e)
+        {
+	        if (!IsVisible)
+		        return;
+
+	        HasMouseFocus = ContainsPoint(e.Position);
+		}
     }
 }
 
--- source/SharpRay/Gui/Button.cs
+++ source/SharpRay/Gui/Button.cs
@@ -2,43 +2,50 @@
 
 namespace SharpRay.Gui
 {
-    public sealed class Button : Label
-    {
-        public Color FocusColor { get; set; } = GRAY;
-        public Color BaseColor { get; set; } = DARKGRAY;
-        public Texture2D? Texture2D { get; set; }
+	public sealed class Button : Label
+	{
+		public Color FocusColor { get; set; } = GRAY;
+		public Color BaseColor { get; set; } = DARKGRAY;
+		public Texture2D? Texture2D { get; set; }
 
-        public override void Render()
-        {
-            FillColor = HasMouseFocus ? FocusColor : BaseColor;
-            
-            if (Texture2D is null)
-                base.Render();
-            else
-                DrawTextureV(Texture2D.Value, Position - Size/2, FillColor);
-        }
+		public override void Render()
+		{
+			if (!IsVisible)
+				return;
 
-        public override bool ContainsPoint(Vector2 point) =>
-                point.X > Position.X - Size.X / 2 &&
-                point.X < Position.X + Size.X / 2 &&
-                point.Y > Position.Y - Size.Y / 2 &&
-                point.Y < Position.Y + Size.Y / 2;
+			FillColor = HasMouseFocus ? FocusColor : BaseColor;
 
-        public override void OnMouseEvent(IMouseEvent e)
-        {
-            base.OnMouseEvent(e);
+			if (Texture2D is null)
+				base.Render( );
+			else
+				DrawTextureV(Texture2D.Value, Position - Size / 2, FillColor);
+		}
 
-            if (!HasMouseFocus || e is not MouseLeftClick mlc) return;
+		public override bool ContainsPoint(Vector2 point) =>
+				point.X > Position.X - Size.X / 2 &&
+				point.X < Position.X + Size.X / 2 &&
+				point.Y > Position.Y - Size.Y / 2 &&
+				point.Y < Position.Y + Size.Y / 2;
 
-            // There are scenarios where we do not want to emit an event, yet still want to invoke the delegate. 
-            // When EmitEvent is null, the delegate won't be invoked even if assigned hence this null check. 
-            if (EmitEvent is not null)
-	            EmitEvent.Invoke(OnMouseLeftClick?.Invoke(this));
-            else
-	            OnMouseLeftClick?.Invoke(this);
+		public override void OnMouseEvent(IMouseEvent e)
+		{
+			if (!IsVisible)
+				return;
 
-            mlc.IsHandled = true;
-        }
-    }
+			base.OnMouseEvent(e);
+
+			if (!HasMouseFocus || e is not MouseLeftClick mlc)
+				return;
+
+			// There are scenarios where we do not want to emit an event, yet still want to invoke the delegate. 
+			// When EmitEvent is null, the delegate won't be invoked even if assigned hence this null check. 
+			if (EmitEvent is not null)
+				EmitEvent.Invoke(OnMouseLeftClick?.Invoke(this));
+			else
+				OnMouseLeftClick?.Invoke(this);
+
+			mlc.IsHandled = true;
+		}
+	}
 }
 
