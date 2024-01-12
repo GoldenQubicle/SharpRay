016940ebe0ff21d334b621dc5018c186994307bd
Erik van Ekeren <e.van.ekeren@betabit.nl>
2024-01-12 22:13:56 +0100

    visualized 2017 day 10

--- source/AoC/KnotHashEntity.cs
+++ source/AoC/KnotHashEntity.cs
@@ -9,38 +9,44 @@ internal class KnotHashEntity(SharpRayConfig config) : AoCEntity(config)
 	private readonly Vector2 _chunkSize = new(config.WindowWidth * MagicNumber, (config.WindowWidth * MagicNumber) / 16);
 
 	private readonly ConcurrentBag<int> _rangeRects = new( );
-	private readonly ConcurrentDictionary<int, int> _list = 
+	private readonly ConcurrentDictionary<int, int> _list =
 		Enumerable.Range(0, 256).ToConcurrentDictionary(n => n, n => n);
 
 	private int _idx;
+	private int _idxNext;
 	private Vector2 _pointer = new(10, 5);
 	private bool _pointerIsMoving;
-	private Vector2 _pointerNext;
+	private readonly bool _showPointer = false;
+	private const int PauseTime = 500;
 
-	private const int PauseTime = 100;
-
+	private int _cycle;
+	private int _op;
 
 	public override async Task RenderAction(IRenderState state, int layer = 0, Color color = default)
 	{
 		var update = state.Cast<KnotHashRender>( );
+		
+		_cycle = update.Cycle;
+		_op = update.Operation;
 
 		await UpdateRange(update.Range);
 
-		var idx = ClampIdx(_idx + update.Jump);
+		_idxNext = ClampIdx(_idx + update.Jump);
 
-		await MovePointer(_anchor + ColRowToScreen(IdxToColRow(idx)));
+		if(_showPointer)
+			await MovePointer( );
 
-		_idx = idx;
+		_idx = _idxNext;
 	}
 
-	
+
 
 	private async Task UpdateRange(List<int> range)
 	{
-		foreach (var t in range.WithIndex())
+		foreach (var t in range.WithIndex( ))
 		{
 			_rangeRects.Add(t.Value);
-			await Task.Delay(AnimationSpeed);
+			//await Task.Delay(AnimationSpeed);
 		}
 
 		foreach (var t in range.WithIndex( ))
@@ -49,20 +55,19 @@ internal class KnotHashEntity(SharpRayConfig config) : AoCEntity(config)
 			await Task.Delay(AnimationSpeed);
 		}
 
-		//await Task.Delay(AnimationSpeed);
+		await Task.Delay(AnimationSpeed);
 		_rangeRects.Clear( );
 	}
 
-	private async Task MovePointer(Vector2 to)
+	private async Task MovePointer()
 	{
 		_pointerIsMoving = true;
-		_pointerNext = to;
 
 		while (_pointerIsMoving)
 			await Task.Delay(AnimationSpeed);
 
-		await Task.Delay(PauseTime);
 		
+		//await Task.Delay(PauseTime);
 	}
 
 	public override void Update(double deltaTime)
@@ -77,23 +82,25 @@ internal class KnotHashEntity(SharpRayConfig config) : AoCEntity(config)
 
 		_pointer.X += _rectSize * 1f;
 
-		var max = ColRowToScreen(IdxToColRow(256)) - new Vector2(_rectSize, 0);
-
-		if (_pointer.X >= max.X && _pointer.Y >= max.Y)
-			_pointer = _anchor;
-
-		if (_pointer.X >= _pointerNext.X && _pointer.Y == _pointerNext.Y)
+		if (ScreenToIdx(_pointer) == _idxNext + 1)
 		{
 			_pointerIsMoving = false;
-			_pointer = _pointerNext;
 			return;
 		}
 
-		if (_pointer.X < _chunkSize.X - _rectSize)
-			return;
 
-		_pointer.X = _anchor.X;
-		_pointer.Y += _chunkHeight + 30;
+		if (_pointer.X > _chunkSize.X)
+		{
+			_pointer.X = _anchor.X;
+			_pointer.Y += _chunkHeight + 30;
+		}
+
+		var max = ColRowToScreen(IdxToColRow(256)) - new Vector2(_rectSize, 0);
+
+		if (_pointer.X >= max.X && _pointer.Y >= max.Y)
+		{
+			_pointer = _anchor;
+		}
 	}
 
 
@@ -105,7 +112,7 @@ internal class KnotHashEntity(SharpRayConfig config) : AoCEntity(config)
 			DrawRectangleLinesV(pos, _chunkSize, Color.GREEN);
 
 			var hash = chunk.Value.Skip(1).Aggregate(chunk.Value[0], (s, t) => s ^ t);
-			DrawTextV(hash.ToString(), pos + new Vector2(_chunkSize.X + 50, _rectSize * .4f), 16, Color.WHITE);
+			DrawTextV(hash.ToString( ), pos + new Vector2(_chunkSize.X + 50, _rectSize * .4f), 16, Color.RAYWHITE);
 
 
 			foreach (var c in chunk.Value.WithIndex( ))
@@ -115,7 +122,7 @@ internal class KnotHashEntity(SharpRayConfig config) : AoCEntity(config)
 			}
 		}
 
-		if(_pointerIsMoving)
+		if(_showPointer)
 			DrawRectangleLinesEx(new Rectangle { Height = _rectSize, Width = _rectSize, X = _pointer.X, Y = _pointer.Y }, 3, Color.ORANGE);
 
 		foreach (var t in _rangeRects.WithIndex( ))
@@ -123,11 +130,16 @@ internal class KnotHashEntity(SharpRayConfig config) : AoCEntity(config)
 			var p = _anchor + ColRowToScreen(IdxToColRow(ClampIdx(_idx + t.idx)));
 			DrawRectangleV(p, new Vector2(_rectSize, _rectSize), ColorAlpha(Color.PURPLE, .25f));
 		}
+
+		DrawTextV($"Cycle {_cycle} / 64", _anchor with { Y = config.WindowHeight - 70 }, 16, Color.RAYWHITE);
+		DrawTextV($"Operation {_op} / 58", _anchor with { Y = config.WindowHeight - 40 }, 16, Color.RAYWHITE);
 	}
+
+	private int ScreenToIdx(Vector2 pos) => (int)Math.Floor(pos.X / _rectSize + pos.Y / (_chunkHeight + 30) * 16);
 
 	private Vector2 ColRowToScreen(Vector2 cr) => new(_rectSize * cr.X, (_chunkHeight + 30) * cr.Y);
 
-	private static Vector2 IdxToColRow(int idx) => new(idx % 16, idx / 16 > 16 ? (idx / 16) - 17 : idx/16);
+	private static Vector2 IdxToColRow(int idx) => new(idx % 16, idx / 16 > 16 ? (idx / 16) - 17 : idx / 16);
 
 	private static int ClampIdx(int idx) => idx > 255 ? idx % 256 : idx;
 }
